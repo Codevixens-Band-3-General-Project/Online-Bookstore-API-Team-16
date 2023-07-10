1c9af409a53ef895bef28a70441d3419b566b2e3
KimFarida <farimomoh@gmail.com>
2023-07-10 13:24:25 +0100

    Farida : Added Mac Connection String Sarah Branch

--- BookstoreAPI/Program.cs
+++ BookstoreAPI/Program.cs
@@ -9,12 +9,18 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 builder.Services.Configure<JWTConfig>(builder.Configuration.GetSection(key: "JwtConfig"));
 builder.Services.AddAuthentication(configureOptions: options =>
 {
