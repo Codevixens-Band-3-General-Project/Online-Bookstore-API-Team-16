6daec5c72ffcbca23cca20af319d409c41b11406
KimFarida <farimomoh@gmail.com>
2023-07-10 17:14:09 +0100

    Added class IdentityInitializer for seeding roles and assiging users

---
+++ BookstoreAPI/Data/IdentityInitializer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+public class IdentityInitializer
+{
+
+    public static async Task SeedRolesAndAssignToUsers(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+        // Seed roles
+        await SeedRoles(roleManager);
+
+        // Assign roles to users
+        await AssignRoles(userManager);
+    }
+
+    public static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+    {
+        if (!await roleManager.RoleExistsAsync("Admin"))
+        {
+            var role = new IdentityRole
+            {
+                Name = "Admin"
+            };
+            await roleManager.CreateAsync(role);
+        }
+
+        if (!await roleManager.RoleExistsAsync("NormalUser"))
+        {
+            var role = new IdentityRole
+            {
+                Name = "NormalUser"
+            };
+            await roleManager.CreateAsync(role);
+        }
+    }
+
+    public static async Task AssignRoles(UserManager<IdentityUser> userManager)
+    {
+        var adminUser = await userManager.FindByEmailAsync("admin@example.com");
+        if (adminUser != null)
+        {
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+
+        var normalUser = await userManager.FindByEmailAsync("user@example.com");
+        if (normalUser != null)
+        {
+            await userManager.AddToRoleAsync(normalUser, "NormalUser");
+        }
+    }
+}
+
+
--- BookstoreAPI/Program.cs
+++ BookstoreAPI/Program.cs
@@ -16,10 +16,10 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
+//    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.Configure<JWTConfig>(builder.Configuration.GetSection(key: "JwtConfig"));
 builder.Services.AddAuthentication(configureOptions: options =>
@@ -45,10 +45,15 @@ builder.Services.AddAuthentication(configureOptions: options =>
 
  }
 );
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+})
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -56,6 +61,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.Use(async (context, next) =>
+{
+    await IdentityInitializer.SeedRolesAndAssignToUsers(context.RequestServices);
+    await next.Invoke();
+});
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
